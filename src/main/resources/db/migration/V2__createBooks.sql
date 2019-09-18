ebf5c7d72005e539e5d163560897c7034e977338
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-18 11:05:37 +0200

    Added some books

---
+++ src/main/resources/db/migration/V2__createBooks.sql
@@ -0,0 +1,7 @@
+create table books (
+   id serial primary key,
+   title varchar(200) null,
+   author varchar(200) null,
+   price integer null,
+   rating integer null
+);
---
+++ src/main/resources/db/migration/V3__addSomeBooks.sql
@@ -0,0 +1,3 @@
+insert into books(title, author, price, rating) values
+('Dziady', 'Mickiewicz', 20, 3),
+('Kordian', 'Slowacki', 30, 5);
