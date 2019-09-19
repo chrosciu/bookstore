6b01aa459ff9a194bb562ebe76174ea16c10fb1e
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-19 11:40:46 +0200

    validation - continued

--- src/main/java/pl/com/sages/bookstore/dto/NewBookDto.java
+++ src/main/java/pl/com/sages/bookstore/dto/NewBookDto.java
@@ -2,13 +2,22 @@ package pl.com.sages.bookstore.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class NewBookDto {
     @NotBlank
     private String author;
+    @NotBlank
     private String title;
+    @Min(1)
+    @Max(100)
+    @NotNull
     private Integer rating;
+    @Min(0)
+    @NotNull
     private Integer price;
 }
