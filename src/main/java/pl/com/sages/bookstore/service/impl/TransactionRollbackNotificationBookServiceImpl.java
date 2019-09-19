a9d7cf8f8503cc216a5402a50ca9b743fca8ec4b
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-19 11:59:32 +0200

    transaction rollback notification

--- src/main/java/pl/com/sages/bookstore/service/impl/TransactionRollbackNotificationBookServiceImpl.java
+++ src/main/java/pl/com/sages/bookstore/service/impl/TransactionRollbackNotificationBookServiceImpl.java
@@ -1,13 +1,15 @@
 package pl.com.sages.bookstore.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.metadata.TypeFactory;
 import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import pl.com.sages.bookstore.dto.BookDto;
 import pl.com.sages.bookstore.dto.NewBookDto;
 import pl.com.sages.bookstore.model.Book;
@@ -18,11 +20,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-//@Primary
-public class ProgrammaticTransactionBookServiceImpl implements BookService {
+@Primary
+@Slf4j
+public class TransactionRollbackNotificationBookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final MapperFacade mapperFacade;
-    private final TransactionTemplate transactionTemplate;
 
     @Override
     public List<BookDto> getAllBooks() {
@@ -35,21 +37,21 @@ public class ProgrammaticTransactionBookServiceImpl implements BookService {
     }
 
     @Override
+    @Transactional
     public BookDto updateBook(int id, NewBookDto newBookDto) {
-        return transactionTemplate.execute(new TransactionCallback<BookDto>() {
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
             @Override
-            public BookDto doInTransaction(TransactionStatus status) {
-                var book = bookRepository.getOne(id);
-                mapperFacade.map(newBookDto, book, null, TypeFactory.valueOf(Book.class));
-                var savedBook = bookRepository.save(book);
-                /*
-                   Force rollback;
-                   Without this automatic commit will take place at end of TransactionCallback
-                 */
-                status.setRollbackOnly();
-                return mapperFacade.map(savedBook, BookDto.class);
+            public void afterCompletion(int status) {
+                if (TransactionSynchronization.STATUS_ROLLED_BACK == status) {
+                    log.info("Book update transaction has been rolled back !");
+                }
             }
         });
+        var book = bookRepository.getOne(id);
+        mapperFacade.map(newBookDto, book, null, TypeFactory.valueOf(Book.class));
+        var savedBook = bookRepository.save(book);
+        throw new NullPointerException();
+        //return mapperFacade.map(savedBook, BookDto.class);
     }
 
     @Override
