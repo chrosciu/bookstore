9ac2d4e3e9a4ec9e64e93a31cbb2cd298e9386d2
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-20 11:36:51 +0200

    tests for create book

--- src/test/java/pl/com/sages/bookstore/service/BookServiceImplTest.java
+++ src/test/java/pl/com/sages/bookstore/service/BookServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import pl.com.sages.bookstore.dto.BookDto;
+import pl.com.sages.bookstore.dto.NewBookDto;
 import pl.com.sages.bookstore.model.Book;
 import pl.com.sages.bookstore.repository.BookRepository;
 import pl.com.sages.bookstore.service.impl.BookServiceImpl;
@@ -22,10 +23,12 @@ public class BookServiceImplTest {
     private BookRepository bookRepository;
     private MapperFacade mapperFacade;
 
+    private static final Book NEW_BOOK = Book.builder().build();
     private static final Book SOME_BOOK = Book.builder().id(1).build();
     private static final Book SOME_OTHER_BOOK = Book.builder().id(2).build();
     private static final BookDto SOME_BOOK_DTO = BookDto.builder().title("A").build();
     private static final BookDto SOME_OTHER_BOOK_DTO = BookDto.builder().title("B").build();
+    private static final NewBookDto SOME_NEW_BOOK_DTO = new NewBookDto();
 
     @Before
     public void init() {
@@ -58,6 +61,20 @@ public class BookServiceImplTest {
 
         //then
         assertThat(books).hasSameElementsAs(List.of(SOME_BOOK_DTO, SOME_OTHER_BOOK_DTO));
+    }
+
+    @Test
+    public void shouldSaveNewBookInRepo(){
+        //given
+        when(mapperFacade.map(SOME_NEW_BOOK_DTO, Book.class)).thenReturn(NEW_BOOK);
+        when(bookRepository.save(NEW_BOOK)).thenReturn(SOME_BOOK);
+        when(mapperFacade.map(SOME_BOOK, BookDto.class)).thenReturn(SOME_BOOK_DTO);
+
+        //when
+        var book = bookService.createBook(SOME_NEW_BOOK_DTO);
+
+        //then
+        assertThat(book).isEqualTo(SOME_BOOK_DTO);
     }
 
 
