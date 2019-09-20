d2ee23efaf1563d792e37dcd5950895df803088e
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-20 08:41:29 +0200

    new pack of tests

--- src/test/java/pl/com/sages/bookstore/service/BookRatingServiceImplTest.java
+++ src/test/java/pl/com/sages/bookstore/service/BookRatingServiceImplTest.java
@@ -1,26 +1,75 @@
 package pl.com.sages.bookstore.service;
 
-import org.junit.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import pl.com.sages.bookstore.model.Book;
+import pl.com.sages.bookstore.model.Review;
 import pl.com.sages.bookstore.service.impl.BookRatingServiceImpl;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+
+@Slf4j
 public class BookRatingServiceImplTest {
+
+    BookRatingService bookRatingService = null;
+
+    @Before
+    public void init() {
+        bookRatingService = new BookRatingServiceImpl();
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionForNullBook() {
-        BookRatingService bookRatingService = new BookRatingServiceImpl();
+        log.info("Inside test");
         var rating = bookRatingService.getMaxRating(null);
     }
 
     @Test
     public void shouldReturnEmptyResultForNoReviews() {
-        BookRatingService bookRatingService = new BookRatingServiceImpl();
         var book = Book.builder().reviews(null).build();
         var rating = bookRatingService.getMaxRating(book);
-        Assert.assertEquals(rating, Optional.empty());
+        assertEquals(rating, Optional.empty());
+    }
+
+    @Test
+    public void shouldReturnEmptyResultForEmptyReviews() {
+        var book = Book.builder().reviews(Collections.emptyList()).build();
+        var rating = bookRatingService.getMaxRating(book);
+        assertEquals(rating, Optional.empty());
+    }
+
+    @Test
+    public void shouldGetMaxRatingIfThereAreAny() {
+        //given
+        var ratings = List.of(1, 4, 7, 6);
+        var reviews = ratings.stream().map(r -> Review.builder().rating(r).build()).collect(toList());
+        var book = Book.builder().reviews(reviews).build();
+
+        //when
+        var rating = bookRatingService.getMaxRating(book);
+
+        //then
+        assertEquals(rating, Optional.of(7));
+    }
+
+    @Test
+    public void shouldGetMaxRatingIfThereAreAny2() {
+        //given
+        var ratings = List.of(1, 4, 6);
+        var reviews = ratings.stream().map(r -> Review.builder().rating(r).build()).collect(toList());
+        var book = Book.builder().reviews(reviews).build();
+
+        //when
+        var rating = bookRatingService.getMaxRating(book);
+
+        //then
+        assertEquals(rating, Optional.of(6));
     }
 
 }
